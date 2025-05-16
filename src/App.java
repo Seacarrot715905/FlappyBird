6688cb650055ce4f4b86e8c021ac05dcc0ec847a
Hurmann <hurmannsaini@gmail.com>
2025-05-15 17:24:33 -0700

    base code

---
+++ src/App.java
@@ -0,0 +1,25 @@
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import javax.swing.*;
+
+public class App {
+
+    public static void main(String[] args) throws Exception {
+
+        JFrame frame = new JFrame("Flappy Bird");
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        int boardWidth = (int) screenSize.getWidth();
+        int boardHeight = (int) screenSize.getHeight();
+        // frame.setVisible(true);
+		frame.setSize(boardWidth, boardHeight);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        FlappyBird flappyBird = new FlappyBird();
+        frame.add(flappyBird);
+        frame.pack();
+        flappyBird.requestFocus();
+        frame.setVisible(true);
+    }
+}
